832a85423dfc69f04991022154e2dbc6693aaca7
PhonqPham <phongvupham1110@gmail.com>
2024-06-02 20:08:00 +0700

    Add Layers

---
+++ src/main/java/vn/edu/fpt/SmartHealthC/controller/AccountController.java
@@ -0,0 +1,7 @@
+package vn.edu.fpt.SmartHealthC.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+}
---
+++ src/main/java/vn/edu/fpt/SmartHealthC/serivce/AccountService.java
@@ -0,0 +1,4 @@
+package vn.edu.fpt.SmartHealthC.serivce;
+
+public interface AccountService {
+}
