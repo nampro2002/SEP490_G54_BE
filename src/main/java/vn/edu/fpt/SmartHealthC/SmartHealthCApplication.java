f7b67ab525b3a9abb15d1706defc26410dd328d0
Nguyen Trung Kien <ntkien651@gmail.com>
2024-06-11 15:31:28 +0700

    Add Swagger into project

--- src/main/java/vn/edu/fpt/SmartHealthC/SmartHealthCApplication.java
+++ src/main/java/vn/edu/fpt/SmartHealthC/SmartHealthCApplication.java
@@ -1,5 +1,7 @@
 package vn.edu.fpt.SmartHealthC;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,12 @@ import vn.edu.fpt.SmartHealthC.serivce.FormQuestionService;
 import vn.edu.fpt.SmartHealthC.serivce.Impl.FormQuestionServiceImpl;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title="SmartHealthC API",
+				version = "1.0",
+				description = "App checking health for old people"
+		)
+)
 public class SmartHealthCApplication {
 
 	public static void main(String[] args) {
