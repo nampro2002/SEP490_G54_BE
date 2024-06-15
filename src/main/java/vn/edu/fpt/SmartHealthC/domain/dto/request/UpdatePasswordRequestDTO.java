eea3148610ec738f4b227178b07a26ecfcfc7876
Nguyen Trung Kien <ntkien651@gmail.com>
2024-06-15 21:40:38 +0700

    Add regex for update password

--- src/main/java/vn/edu/fpt/SmartHealthC/domain/dto/request/UpdatePasswordRequestDTO.java
+++ src/main/java/vn/edu/fpt/SmartHealthC/domain/dto/request/UpdatePasswordRequestDTO.java
@@ -2,6 +2,7 @@ package vn.edu.fpt.SmartHealthC.domain.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +14,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UpdatePasswordRequestDTO {
     @NotBlank(message = "missing oldPassword")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be minimum 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character."
+    )
     private String oldPassword;
     @NotBlank(message = "missing newPassword")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be minimum 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character."
+    )
     private String newPassword;
 }
