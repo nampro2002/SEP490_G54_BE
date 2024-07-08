97ce49326ede1095ac8fefd9163712cf85678f44
bsd03rd <bsd03rd@gmail.com>
2024-07-08 09:21:27 +0700

    Add test_case updateActivityRecord(ACTIVITY_PLAN_NOT_FOUND)

--- src/test/java/vn/edu/fpt/SmartHealthC/serivce/Impl/ActivityRecordServiceImplTest.java
+++ src/test/java/vn/edu/fpt/SmartHealthC/serivce/Impl/ActivityRecordServiceImplTest.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import vn.edu.fpt.SmartHealthC.domain.Enum.TypeMedicalHistory;
 import vn.edu.fpt.SmartHealthC.domain.dto.request.ActivityRecordCreateDTO;
+import vn.edu.fpt.SmartHealthC.domain.dto.request.ActivityRecordUpdateDTO;
 import vn.edu.fpt.SmartHealthC.domain.entity.*;
 import vn.edu.fpt.SmartHealthC.exception.AppException;
 import vn.edu.fpt.SmartHealthC.exception.ErrorCode;
@@ -56,6 +57,8 @@ public class ActivityRecordServiceImplTest {
     private ActivityRecord activityRecord ;
 
     private List<Date> dateList;
+
+    private ActivityRecordUpdateDTO activityRecordDTO ;
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
@@ -71,6 +74,8 @@ public class ActivityRecordServiceImplTest {
         dateList.add(new Date());
         dateList.add(new Date(System.currentTimeMillis() - 86400000));
         dateList.add(new Date(System.currentTimeMillis() + 86400000));
+
+        activityRecordDTO = new ActivityRecordUpdateDTO();
     }
 
 
@@ -197,5 +202,33 @@ public class ActivityRecordServiceImplTest {
 
     }
 
+    //more ...
 
+    @Test
+    void updateActivityRecord_ACTIVITY_PLAN_NOT_FOUND() {
+
+        //Given
+        when(appUserRepository.findByAccountEmail("test@test.com")).thenReturn(Optional.of(testAppUser));
+        when(activityRecordRepository.findRecordByIdUser(testAppUser.getId())).thenReturn(activityRecordList);
+        when(activityRecordRepository.findById(1)).thenReturn(Optional.of(activityRecord));
+        when(activityRecordRepository.save(activityRecord)).thenReturn(activityRecord);
+
+
+        Authentication mockAuthentication = Mockito.mock(Authentication.class);
+        SecurityContext mockSecurityContext = Mockito.mock(SecurityContext.class);
+        SecurityContextHolder.setContext(mockSecurityContext);
+
+        when(mockSecurityContext.getAuthentication()).thenReturn(mockAuthentication);
+        when(mockAuthentication.getName()).thenReturn("Test@gmail.com");
+
+
+        List<ActivityRecord> resultActivityRecords = activityRecordRepository.findRecordByIdUser(testAppUser.getId());
+        assertNotNull(resultActivityRecords);
+
+        AppException exception = assertThrows(AppException.class,
+                () -> activityRecordService.updateActivityRecord(activityRecordDTO));
+
+        assertEquals(ErrorCode.APP_USER_NOT_FOUND, exception.getErrorCode());
+
+    }
 }
