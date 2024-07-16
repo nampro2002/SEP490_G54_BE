d57e700225e36452e7a949ccf2320f0f316c5c8f
Bach Duc <bsd03rd>
2024-07-16 13:27:06 +0700

    Fix api forgot password

--- src/main/java/vn/edu/fpt/SmartHealthC/controller/ForgetPasswordCodeController.java
+++ src/main/java/vn/edu/fpt/SmartHealthC/controller/ForgetPasswordCodeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import vn.edu.fpt.SmartHealthC.domain.dto.request.ChangePasswordCodeDTO;
 import vn.edu.fpt.SmartHealthC.domain.dto.request.ForgetPasswordCodeDTO;
 import vn.edu.fpt.SmartHealthC.domain.dto.response.ApiResponse;
 import vn.edu.fpt.SmartHealthC.serivce.ForgetPasswordCodeService;
@@ -36,11 +37,20 @@ public class ForgetPasswordCodeController {
     }
 
     @PostMapping("/email/verify")
-    public ApiResponse<Boolean> verifyCodeAndChangePassword(@RequestBody @Valid ForgetPasswordCodeDTO forgetPasswordCodeDTO) throws ParseException {
+    public ApiResponse<Boolean> checkEmailCodeInvalid(@RequestBody @Valid ForgetPasswordCodeDTO forgetPasswordCodeDTO) throws ParseException {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponse.<Boolean>builder()
                         .code(HttpStatus.OK.value())
-                        .result(forgetPasswordCodeService.verifyAndChangePassword(forgetPasswordCodeDTO))
+                        .result(forgetPasswordCodeService.checkEmailCodeInvalid(forgetPasswordCodeDTO))
+                        .build()).getBody();
+    }
+
+    @PostMapping("/email/change-password")
+    public ApiResponse<Boolean> verifyCodeAndChangePassword(@RequestBody @Valid ChangePasswordCodeDTO changePasswordCodeDTO) throws ParseException {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.<Boolean>builder()
+                        .code(HttpStatus.OK.value())
+                        .result(forgetPasswordCodeService.verifyAndChangePassword(changePasswordCodeDTO))
                         .build()).getBody();
     }
 
--- src/main/java/vn/edu/fpt/SmartHealthC/domain/dto/request/ChangePasswordCodeDTO.java
+++ src/main/java/vn/edu/fpt/SmartHealthC/domain/dto/request/ChangePasswordCodeDTO.java
@@ -2,25 +2,21 @@ package vn.edu.fpt.SmartHealthC.domain.dto.request;
 
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import vn.edu.fpt.SmartHealthC.domain.Enum.TypeActivity;
-
-import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ForgetPasswordCodeDTO {
+public class ChangePasswordCodeDTO {
     @NotBlank(message = "missing email")
     private String email;
-    @NotBlank(message = "missing code")
-    private String code;
-//    @NotBlank(message = "missing password")
-//    private String password;
+//    @NotBlank(message = "missing code")
+//    private String code;
+    @NotBlank(message = "missing password")
+    private String password;
 
 }
