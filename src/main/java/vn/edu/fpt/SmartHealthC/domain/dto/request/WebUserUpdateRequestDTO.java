d7c638b5cb564102ddc159c82247196b6377678c
nampro2002 <nampro2k2@gmail.com>
2024-08-07 11:39:08 +0700

    update regex for update web user

--- src/main/java/vn/edu/fpt/SmartHealthC/domain/dto/request/WebUserUpdateRequestDTO.java
+++ src/main/java/vn/edu/fpt/SmartHealthC/domain/dto/request/WebUserUpdateRequestDTO.java
@@ -2,6 +2,7 @@ package vn.edu.fpt.SmartHealthC.domain.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +21,10 @@ public class WebUserUpdateRequestDTO {
     private Date dob;
     @NotNull(message = "missing gender")
     private Boolean gender;
+    @Pattern(
+            regexp = "^0\\d{8,10}$",
+            message = "Phone number must start with 0 and has 8-10 digits following"
+    )
     @NotBlank(message = "missing phoneNumber")
     private String phoneNumber;
 }
